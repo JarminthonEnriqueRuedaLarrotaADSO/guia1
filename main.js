cbca0152fe4d9f7205eaaabb6e66143243da5c27
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-07 11:39:46 -0500

    Ejercicio 11

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // . Escribir un algoritmo que permita obtener las raíces reales de la ecuación de segundo grado: A * x2 + b * x + c, siendo X un valor constante.
-
+    // Escribir un programa que calcule el volumen de un elipsoide
+    // V = (4/3) * PI * a * b *c
    
-    let valorX = Number(prompt("Ingrese el valor de X ", ));
     let valorA = Number(prompt("Ingrese el valor de A ", ));
     let valorB = Number(prompt("Ingrese el valor de B ", ));
     let valorC = Number(prompt("Ingrese el valor de C ", ));
 
-    let Formula =(valorA * valorX*2 + valorB * valorX + valorC) ;
-    console.log(`%cFormula (${'A * x2 + b * x + c'} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`El volumen de la esfera es : "${Formula}"`);
+    let Formula =(4/3) * Math.PI * valorA * valorB * valorC ;
+    console.log(`%cFormula (${'(4/3) * PI * a * b *c'} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`El volumen de la elipsoide es : "${Formula}"`);
 
 })
