d80f5ec296ca86ca8865eed1572733cb424107d1
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-07 10:52:48 -0500

    Ejercicio 8

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Escribir un programa que calcule el volumen de una esfera:
-    // Radio = 3 volumen de la esfera = 4/3 * PI * radio˄3
+    // Escribir un programa que evalúe la siguiente expresión:
+    // (a+7*c)/(b+2-a)+2*b 
    
-    let radio = Number(prompt("Ingrese el volumen del radio", 3));
+    let a = Number(prompt("Ingrese el valor de 'a' ", ));
+    let b = Number(prompt("Ingrese el valor de 'b' ", ));
+    let c = Number(prompt("Ingrese el valor de 'c' ", ));
 
-    let volumenEsfera =(4/3*Math.PI*Math.pow(radio, 3)) ;
-    console.log(`%cFormula (${4} / ${3} * ${'Pi'} * ${'radio**3'})`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-    console.log(`El volumen de la esfera es : "${volumenEsfera}"`);
+    let Formula =((a+7*c)/(b+2-a)+2*b) ;
+    console.log(`%cFormula (${(a+7*c)} / ${(b+2-a)} + ${'2*b'})`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+    console.log(`El volumen de la esfera es : "${Formula}"`);
 
 })
