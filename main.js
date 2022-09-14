00ac57fef28024fb05c56f9090e8276c8e95eddd
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-14 10:15:15 -0500

    Ejercicio 16

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 15. Hacer un diagrama para convertir de grados centígrados a grados Fahrenheit
-    
-    let gradosC = Number(prompt("Ingrese los grados centigrados para transformar a farenheit: ", ));
-    let conversion=(gradosC * 9/5) + 32
-   
-    console.log(`%cFormula (${'(C° * 9/5) + 32'} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`Sus grados farenheit son:   "${conversion}"`);
+    // 16. Elaborar un algoritmo para calcular el promedio final de la materia de algoritmos. Dicha calificación se
+    // compone de los siguientes porcentajes:
+    // • 55% del promedio final de sus calificaciones de los tres(3) parciales.
+    // • 30% de la calificación examen final y
+    // • 15% de la calificación trabajo final.
+
+    let nota1 = Number(prompt("Ingrese la nota del parcial1:  ", ));
+    let nota2 = Number(prompt("Ingrese la nota del parcial2: ", ));
+    let nota3 = Number(prompt("Ingrese la nota del parcial3: ", ));
+    let nota4 = Number(prompt("Ingrese la nota del examen final: ", ));
+    let nota5 = Number(prompt("Ingrese la nota del trabajo final: ", ));
+    let sumPromedio=(nota1+nota2+nota3)/3*0.55
+    let examf= nota4*0.30
+    let trabajof= nota5*0.15
+    let sumafinal= sumPromedio+examf+trabajof
+    console.log(`%cFormula (${'sumar el porcentaje de las notas'} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`Su nota final es:   "${sumafinal}"`);
 })
