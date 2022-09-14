84513ce158e3d60066dd103535f2cfbce84104f1
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-14 10:59:04 -0500

    Ejercicio 19

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 18. Calcular el sueldo de un empleado dados comodatos de entrada: el nombre, hrs. De trabajo y el pago
-    // en hora. Pagohora=15300
-    let Trabajador = (prompt("Ingrese su nombre:  ", ));
-    let horas = Number(prompt("ingrese las horas trabajadas:  ", ));
-    let Pagohora=15300
-    let calculoSueldo= horas*15300
-    console.log(`%cFormula (${'se multiplican horas trabajadas*pagohora(15300) '} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`señor "${Trabajador}"`);
-    console.log(`El valor de su salario segun sus horas trabajadas es de:   "${calculoSueldo}"`);
+    // 19. Un estudiante realiza cuatro exámenes. Realizar el pseudocódigo que representen el algoritmo
+    // correspondiente para obtener el promedio de las calificaciones obtenidas. las calificaciones van 1 a 5
+    // puntos.
+    let nota1 = Number(prompt("Ingrese su calificacion de 1 a 5:  ", ));
+    let nota2 = Number(prompt("Ingrese su calificacion de 1 a 5:  ", ));
+    let nota3 = Number(prompt("Ingrese su calificacion de 1 a 5:  ", ));
+    let nota4 = Number(prompt("Ingrese su calificacion de 1 a 5:  ", ));
+    let promedio=(nota1+nota2+nota3+nota4)/4
+    console.log(`%cFormula (${'Se suman las notas y se dividen en la cantidad de notas '} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`Su calificacion es de: "${promedio}"`);
+    
 })
