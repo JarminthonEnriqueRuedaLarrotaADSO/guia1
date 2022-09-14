e01adcfed62eea576cac2d0642233ef11588a3ee
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-14 09:59:34 -0500

    Ejercicio 15

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 14. Una farmacia aplica al precio de los remedios el 10% de descuento, hacer un programa que
-    // ingresando el costo de los medicamentos calcules el descuento y el precio final.
+    // 15. Hacer un diagrama para convertir de grados centígrados a grados Fahrenheit
     
-    let producto = Number(prompt("Ingrese el valor del producto para calcular el 10% de descuento: ", ));
-    let descuento=producto*0.10
-    let aplicacionDescuento=producto-descuento
+    let gradosC = Number(prompt("Ingrese los grados centigrados para transformar a farenheit: ", ));
+    let conversion=(gradosC * 9/5) + 32
    
-    console.log(`%cFormula (${'(producto*0.10)--la multiplicacion anterior se resta al valor original del producto '} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`su descuento es de:  "${descuento}"`);
-    console.log(`Su valor a pagar es de  "${aplicacionDescuento}"`);
+    console.log(`%cFormula (${'(C° * 9/5) + 32'} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`Sus grados farenheit son:   "${conversion}"`);
 })
