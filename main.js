b00bf6109a6c81e1de70e19d0370f1b46770e836
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-14 09:12:55 -0500

    Ejercicio 14

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 13. Realice un algoritmo que a partir de proporcionarle la velocidad de un automóvil expresada en
-    // kilómetros por hora, proporcione la velocidad en metros por segundos.
+    // 14. Una farmacia aplica al precio de los remedios el 10% de descuento, hacer un programa que
+    // ingresando el costo de los medicamentos calcules el descuento y el precio final.
     
-    let km = Number(prompt("Ingrese los km que quiere transformar a metros: ", ));
-    let conver=km/3.6
+    let producto = Number(prompt("Ingrese el valor del producto para calcular el 10% de descuento: ", ));
+    let descuento=producto*0.10
+    let aplicacionDescuento=producto-descuento
    
-    console.log(`%cFormula (${'los km se dividen en 3.600 '} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`Sus mt/s son:  "${conver}"`);
+    console.log(`%cFormula (${'(producto*0.10)--la multiplicacion anterior se resta al valor original del producto '} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`su descuento es de:  "${descuento}"`);
+    console.log(`Su valor a pagar es de  "${aplicacionDescuento}"`);
 })
