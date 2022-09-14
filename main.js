43f8e606fe6cabbba41c85fe63ce6f604999ebac
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-14 11:24:00 -0500

    Ejercicio 21

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 20. Un vendedor recibe un sueldo base más el 10% de comisión sobre sus ventas. Si en un mes
-    // cualquiera hace tres ventas por valores: v1, v2 y v3, ¿cuánto recibirá por comisión? y ¿cuánto en total
-    // sueldo del vendedor?.
-    let venta1 = Number(prompt("Ingrese el valor de la venta 1:  ", ));
-    let venta2 = Number(prompt("Ingrese el valor de la venta 2:  ", ));
-    let venta3 = Number(prompt("Ingrese su valor de la venta 3:  ", ));
-    let promedio=(venta1+venta2+venta3)
-    let valor=promedio*0.10
-    console.log(`%cFormula (${'Se suman las notas y se dividen en la cantidad de notas '} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`El valor de las ventas es: "${promedio}"`);
-    console.log(`Su valor de comision por la compra es de : "${valor}" mas su sueldo base`);
+    // 21. Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución,
+    // por favor utilizar funciones matemáticas de Python para ello
+    // d = rc((x2-x1)^2 + (y2-y1)^2)
+    let valX = Number(prompt("Ingrese el valor de x:  ", ));
+    let valY = Number(prompt("Ingrese el valor de y:  ", ));
+    let valRC = Number(prompt("Ingrese el valor de R:  ", ));
+    let solucion= valRC*((valX*(2)-valX*(1))**2 + (valY*(2)-valY*(1))**2)
+    console.log(`%cFormula (${'d = rc((x2-x1)^2 + (y2-y1)^2)'} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`La solucion de la formula es : "${solucion}"`);
     
 })
