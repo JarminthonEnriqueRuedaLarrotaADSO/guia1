a938e9116c7a9646da3f15efd23e8a3862fbe79b
JarminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-09-14 10:52:08 -0500

    Ejercicio 18

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 17. Dado el valor que un cliente paga por un producto, calcular qué valor corresponde al costo total del
-    // producto y cuánto es el valor del IVA. Considerando que el porcentaje del IVA puede variar en el
-    // tiempo y de un producto a otro, este dato se lee por teclado.
-    let producto = Number(prompt("Ingrese el costo del producto:  ", ));
-    let Iva = Number(prompt("Ingrese el valor del IVA en numero entero : ", ));
-    let convertirIva= Iva/100
-    let smf= producto*convertirIva
-    let sumafinal=producto+smf
-    console.log(`%cFormula (${'sumar el IVA al valor del producto: '} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
-    console.log(`El valor original del producto es:   "${producto}"`);
-    console.log(`El IVA es:   "${convertirIva}"`);
-    console.log(`El valor final del producto con el IVA es:   "${sumafinal}"`);
+    // 18. Calcular el sueldo de un empleado dados comodatos de entrada: el nombre, hrs. De trabajo y el pago
+    // en hora. Pagohora=15300
+    let Trabajador = (prompt("Ingrese su nombre:  ", ));
+    let horas = Number(prompt("ingrese las horas trabajadas:  ", ));
+    let Pagohora=15300
+    let calculoSueldo= horas*15300
+    console.log(`%cFormula (${'se multiplican horas trabajadas*pagohora(15300) '} )`, `background-color: black; color: aliceblue;border: blue dashed 3px; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; border-radius: 5px;`);
+    console.log(`señor "${Trabajador}"`);
+    console.log(`El valor de su salario segun sus horas trabajadas es de:   "${calculoSueldo}"`);
 })
